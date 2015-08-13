0ca3c21f16610008450100a4a2551ff03dc7fec0
dudusbarros <joseeduardobarros@gmail.com>
2015-08-13 19:37:57 -0300

    Add styles Create home Minify images

--- gulpfile.js
+++ gulpfile.js
@@ -3,9 +3,12 @@ var gulp = require('gulp'),
 	jshint = require('gulp-jshint'),
 	minifycss = require('gulp-minify-css'),
 	rename = require('gulp-rename'),
-	uglify = require('gulp-uglify');
+	uglify = require('gulp-uglify'),
+	gls = require('gulp-live-server'),
+	tinypng = require('gulp-tinypng');
 
 var cssFiles = 'css/*.css',
+	imgfiles = 'img/*',
 	jsFiles = 'js/villa.js';
 
 gulp.task('css', function() {
@@ -29,6 +32,31 @@ gulp.task('js', function() {
 			extname: '.min.js'
 		}))
 		.pipe(gulp.dest('dist/js'));
+});
+
+gulp.task('tinypng', function () {
+	gulp.src(imgfiles)
+		.pipe(tinypng('8eNoFlUv4wHzam_8GleKHdhH2YFk9xAd'))
+		.pipe(gulp.dest('dist/img'));
+});
+
+gulp.task('serve', function() {
+	//1. serve with default settings
+	//var server = gls.static(); //equals to gls.static('public', 3000);
+	//server.start();
+
+	//2. serve at custom port
+	var server = gls.static('/', 8808);
+	server.start();
+
+	//3. serve multi folders
+	//var server = gls.static(['/'], 8888);
+	//server.start();
+
+	//use gulp.watch to trigger server actions(notify, start or stop)
+	gulp.watch(['index.html', 'clean/*.html'], function () {
+		server.notify.apply(server, arguments);
+	});
 });
 
 gulp.task('default', function() {
